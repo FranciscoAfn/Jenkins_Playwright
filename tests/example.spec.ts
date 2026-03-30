6e2292a77841d62e53d280581649751c7e14c023
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-03-30 13:49:45 +0100

    Update test to use Google instead of Example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example/);
+  await page.goto('https://google.com');
+  await expect(page).toHaveTitle(/Google/);
 });
