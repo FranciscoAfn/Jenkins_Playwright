2c8cb34ee168961f5fd4a1e7b11c0ca77574ff40
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-03-31 14:10:15 +0100

    Rename test from 'basic test' to 'example test'

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('example test', async ({ page }) => {
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
 });
