bcd50b933b35d2a78d2242557da3bdac1e2e26b6
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-03-31 11:30:38 +0100

    Change reporter to open HTML report automatically

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   reporter: [
-    ['html', { outputFolder: 'playwright-report', open: 'never' }]
+    ['html', { outputFolder: 'playwright-report', open: 'always' }]
   ],
 });
