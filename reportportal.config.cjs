e1b9eeb560074ff94fc8b5ff9d0aa76d561442b6
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-04-07 14:30:58 +0100

    Change endpoint URL format in config
    Updated endpoint URL to remove the protocol.

--- reportportal.config.cjs
+++ reportportal.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   apiKey: "Jenkins_4wtt6XlzSRKCu5LIril4uDZqT3LtROGjHyUlpXrHD3GvSxO90JKJtDYtXTjG3lO1",
-  endpoint: "https://localhost:8080/api/v2",
+  endpoint: "localhost:8080/api/v2",
   project: "superadmin_personal",
   launch: "Playwright CI Run",
   description: "Playwright tests executed in Jenkins",
