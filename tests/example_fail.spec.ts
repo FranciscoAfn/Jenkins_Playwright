db4d0ce200d37643c20bef084381c98e9a8be028
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-04-10 14:29:02 +0100

    Update example_fail.spec.ts

--- tests/example_fail.spec.ts
+++ tests/example_fail.spec.ts
@@ -1,1 +1,6 @@
-import
+import { test, expect } from '@playwright/test';
+
+test('example test @example_fail', async ({ page }) => {
+  await page.goto('https://google.com');
+  await expect(page).toHaveTitle(/Apple/);
+});
