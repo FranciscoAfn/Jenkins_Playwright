a00cc7e4e856dd6db997cc27603a5bcde266bace
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-04-10 18:04:58 +0100

    Update example.spec.ts

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../fixtures/base-test';
 
-test('example test @example', async ({ page }) => {
+test('example test @test', async ({ page }) => {
   await page.goto('https://google.com');
   await expect(page).toHaveTitle(/Google/);
 });
