13742e6edcd718260730d76f2ea1e771a3ebd7ab
FranciscoAfn <f.afonso@accredit-solutions.com>
2026-04-10 14:00:51 +0100

    Add device projects for Chromium, Firefox, and Webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 import rpConfig from './reportportal.config.cjs';
 
 export default defineConfig({
   testDir: './tests',
+    projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
   reporter: [
     ['@reportportal/agent-js-playwright', rpConfig]
   ]
